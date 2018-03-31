5a061ad7168f06832515ae3c199e264bf60ab888
ilie-vlad <vlad.iulian.ilie@gmail.com>
2018-03-31 10:56:13 +0300

    graphical user interface

---
+++ Bearing.m
@@ -0,0 +1,13 @@
+classdef Bearing
+    properties
+        BPFI        %ball pass frequency inner race
+        BPFO        %ball pass frequency outer race
+        FTF         %fundamental train frequency
+        BPF         %ball pass frequency
+        N           %number of balls
+        
+    end
+    
+    
+    
+end
---
+++ loadBearings.m
@@ -0,0 +1,9 @@
+function [ bearings ] = loadBearings(  )
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+
+    
+    
+
+end
+
