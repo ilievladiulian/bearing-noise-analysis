5d04bceda9ab3bb42ac39588fa6ea4bb1e87d201
ilie-vlad <vlad.iulian.ilie@gmail.com>
2018-04-14 12:12:18 +0300

    Fixed spectrum generation: - removed double fft on signal - changed the way the frequency vector is generated - removed shifting of signal representation so the frequency starts from 0 - added limits on OX axis to 0 - 100 Hz

--- plot_t.m
+++ plot_t.m
@@ -1,16 +1,16 @@
-function plot_S(Te,s,graphics) 
+function plot_t(Te,s,graphics) 
 % Te reprezinta perioada de esantionare in secunde 
 % s este vectorul care contine esantioanele            
     N=max(size(s)); % se presupune ca este vb de un vector cu mai mult                         
                     % de 1 element                 
     fe=1/Te;    % frecventa de esantionare [Hz]          
-    f = (0:N-1)*(fe/N);  % vectorul de frecvente - acesta are                                   
+    f = (0:N-1)/fe;  % vectorul de frecvente - acesta are                                   
                               % acelasi numar de componente la fel ca                                    
                               % semnalul.                                   
                               % - s-a cautat centrarea lui in origine                      
     %S=fft(s);     % se calculeaza TFD cu FFT              
-    S=abs(s);     % se reprezinta doar modulul TFD         
-    plot(graphics,f,10*log10(S));
-    graphics.XLim = [0 100];
+    %S=abs(S);     % se reprezinta doar modulul TFD         
+    plot(graphics,f,s);
+    % graphics.XLim = [0 300];
 end 
  
