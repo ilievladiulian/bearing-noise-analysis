59e8255c06dc5f3ddba07d7744a7842224a02964
Rosana-Constantin <Rosana.12mai>
2018-05-16 21:17:30 +0300

    added bearings dimensions in library

--- loadBearings.m
+++ loadBearings.m
@@ -3,17 +3,17 @@ function [ bearings ] = loadBearings(  )
 %   Detailed explanation goes here
     import Bearing.*
        
-    bearing1 = Bearing(6, 16, degtorad(0), 1);
+    bearing1 = Bearing(5, 15, degtorad(0), 1);
     bearing1 = setOrbitalFrequency(bearing1);
     bearing1 = setRotationFrequency(bearing1);
     
     
-    bearing2 = Bearing(5, 15, degtorad(0), 2);
+    bearing2 = Bearing(6, 16, degtorad(0), 2);
     bearing2 = setOrbitalFrequency(bearing2);
     bearing2 = setRotationFrequency(bearing2);
     
     
-    bearing3 = Bearing(5, 15, degtorad(0), 3);
+    bearing3 = Bearing(4.5, 23.5, degtorad(0), 3);
     bearing3 = setOrbitalFrequency(bearing3);
     bearing3 = setRotationFrequency(bearing3);
     
